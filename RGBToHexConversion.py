7a1a55a18e880425cad41f7b5764538a291ae5a3
Hossein Karagah <42470308+hkaragah@users.noreply.github.com>
2020-08-06 00:27:25 -0700

    Update RGBToHexConversion.py

--- RGBToHexConversion.py
+++ RGBToHexConversion.py
@@ -51,7 +51,11 @@ def rgb(r, g, b):
 print(rgb(0,0,0))
 print(rgb(255,255,255))
 
+# Best Practice on CodeWars (not my solution)
 
+##def rgb(r, g, b):
+##    round = lambda x: min(255, max(x, 0))
+##    return ("{:02X}" * 3).format(round(r), round(g), round(b))
 
 
 
